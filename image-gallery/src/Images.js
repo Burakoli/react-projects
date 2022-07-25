e25045ac381a5ae0e57997933cba8b663c4827d2
Burak Aydemir <aydemir_0730@hotmail.com>
2022-07-25 15:39:38 +0300

    Gallery images added.

---
+++ image-gallery/src/Images.js
@@ -0,0 +1,12 @@
+export default [
+    "https://www.kuuasema.com/wp-content/uploads/2020/06/1200px-Electronic-Arts-Logo.svg-2-300x300.png",
+    "https://images.apiservisleri.com/images/riot.png",
+    "https://brandlogos.net/wp-content/uploads/2013/07/atari-black-vector-logo.png",
+    "https://upload.wikimedia.org/wikipedia/commons/c/c1/Steam_Logo.png",
+    "https://keymailer.co/wp-content/uploads/sites/28/2021/10/Epic-Games-White-S2-300x300.png",
+    "https://cdn-600605d4c1ac18031c64d726.closte.com/wp-content/uploads/2021/04/Xiaomi.png",
+    "https://www.logovector.org/wp-content/uploads/2018/12/whatsapp_logo-300x300.png",
+    "https://www.brandingturkiye.com/wp-content/uploads/2018/07/bmw-logo-anlami-300x300.png",
+    "https://www.brandingturkiye.com/wp-content/uploads/2018/07/volkswagen-logo-300x282.png",
+    "https://www.brandingturkiye.com/wp-content/uploads/2018/07/Fiat-logo-300x287.png",
+];
