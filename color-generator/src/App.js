9b41729b8d85539c52cdf4a518c3f3a1ba23e4c6
Burak Aydemir <aydemir_0730@hotmail.com>
2022-07-25 13:45:34 +0300

    Color generator completed
    The color generator project has been completed.

---
+++ color-generator/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [boxes, setBoxes] = useState ([{ bg: "#" + ((Math.random() * 0xffffff) << 0).toString(16) }]);
+  
+  useEffect(() => {
+    
+    const interval = setInterval (() => {
+      setBoxes([...boxes, { bg: "#" + ((Math.random() * 0xffffff) << 0).toString(16) }]);
+    }, 100);
+    return () => clearInterval(interval);
+  
+  }, [boxes]);
+
+  return (
+    <div className="hero">
+      {boxes.map((box, index) => (
+          <div key={index} className="box" style={{ background: box.bg }}>
+            <p>{index} <br/> <br/> {(box.bg).toString(16)}</p>
+          </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
