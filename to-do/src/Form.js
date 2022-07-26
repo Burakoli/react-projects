a4f674825489b70ae09213457ce92be48e069ce8
Burak Aydemir <aydemir_0730@hotmail.com>
2022-07-26 12:22:48 +0300

    The form has been created.

---
+++ to-do/src/Form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Form = () => {
+    
+    return(
+        <form>
+            <input type="text" placeholder="Add to-do..." />
+            <button type="submit">Add to-do</button>
+        </form>
+    );
+};
+
+export default Form;
