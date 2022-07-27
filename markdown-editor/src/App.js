0fd14ce12cba2a7a7ec2ca1cdaf440ce584e6587
Burak Aydemir <aydemir_0730@hotmail.com>
2022-07-27 11:26:26 +0300

    Input and output text parts are completed.
    Package "react-markdown" downloaded via npm is suspended for various security reasons on HTML. Therefore, the program cannot convert the text properly.

--- markdown-editor/src/App.js
+++ markdown-editor/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown"; 
+import "./App.css";
 
 function App() {
+  const [value, setValue] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="hero">
+      <div className="markdownContainer">
+        <textarea value={value} onChange={e => setValue(e.target.value)} />
+        <ReactMarkdown className="output" source={value} escapeHtml={false} />
+      </div>
     </div>
   );
 }
