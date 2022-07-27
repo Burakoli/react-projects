4464674c597ff4eaa4aa6057c0edb7d83de2b9b4
Burak Aydemir <aydemir_0730@hotmail.com>
2022-07-27 15:30:44 +0300

    Container created and featured.

---
+++ weather-app/src/components/Container.js
@@ -0,0 +1,19 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+
+const Container = ({ children }) => (
+    <div css={styles} className="container" >
+        { children }
+    </div>
+);
+
+const styles = css`
+    width: 100%;
+    max-width: 1000px;
+    margin: auto;
+    @media (max-width: 1040px) {
+        max-width: 90%;
+    }
+`;
+
+export default Container;
