689458bf4e2aba3a2b43e2b568d5d02dfd0cd68a
Burak Aydemir <aydemir_0730@hotmail.com>
2022-07-27 15:37:28 +0300

    Container, WeatherCard imported and added.

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Container from './components/Container';
+import WeatherCard from './components/WeatherCard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <WeatherCard />
+      </Container>
     </div>
   );
 }
